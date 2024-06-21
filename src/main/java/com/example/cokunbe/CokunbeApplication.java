9d77ffd9e347d415a1da8da052486118bc99ae69
gyesswhat <h2weirdo@icloud.com>
2024-06-21 14:41:17 +0900

    chore: 기본 파일 세팅

---
+++ src/main/java/com/example/cokunbe/CokunbeApplication.java
@@ -0,0 +1,13 @@
+package com.example.cokunbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CokunbeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CokunbeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/cokunbe/CokunbeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cokunbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CokunbeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
